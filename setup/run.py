0b899bedf4d7a8eead8e03d2d63a91345da83125
gmso <german.mene@gmail.com>
2021-10-27 13:49:59 +0200

    Attempt self kill of setup files

--- setup/run.py
+++ setup/run.py
@@ -1,7 +1,20 @@
+import sys, subprocess
+
 from user_config import get_user_config
 from poetry_setup import setup_poetry
 from pytest_setup import setup_pytest
 from django_project_setup import create_django_project 
+
+
+def destroy_setup():
+    """Destroy the setup folder with all its content, including this script"""
+    subprocess.Popen(
+        "python -c \""
+        "import shutil, time; "
+        "time.sleep(1); "
+        f"shutil.rmtree('os.path.dirname({sys.argv[0]}');\""
+    )
+    sys.exit(0)
 
 
 def main():
@@ -10,6 +23,7 @@ def main():
     setup_poetry(config)
     setup_pytest(config)
     create_django_project(config)
+    destroy_setup()
 
 
 if __name__ == "__main__":
