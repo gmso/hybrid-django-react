955828c9f8f85d672b1467983587051b98a07d52
gmso <german.mene@gmail.com>
2021-10-29 17:42:07 +0200

    Add index/home url

--- setup/urls_setup.py
+++ setup/urls_setup.py
@@ -17,7 +17,8 @@ def change_project_urls(config):
         f.write(
             'from django.conf import settings\n'
             'from django.contrib import admin\n'
-            'from django.urls import path, include\n\n'
+            'from django.urls import path, include\n'
+            'from django.views.generic import TemplateView\n\n'
             'import debug_toolbar\n\n'
             'urlpatterns = [\n'
             '    # Django admin:\n'
@@ -25,6 +26,7 @@ def change_project_urls(config):
             '    # User management:\n'
             '    #path("accounts/", include("allauth.urls")),\n'
             '    # Local apps:\n'
+            '    #path("/", TemplateView.as_view(template_name="index.html")),\n'
             ']\n\n'
             'if settings.DEV_MODE:\n'
             '    import debug_toolbar\n\n'
