88fca24122bf96957bbc483394930c60543128cd
gmso <german.mene@gmail.com>
2021-10-30 12:55:31 +0200

    Add React and React-DOM

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
-  document.body.appendChild(component());
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Hello, react!</h1>,
+  document.getElementById('root')
+);
