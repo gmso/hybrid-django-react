3ad6fbf652f59a8cb90e1c455c71dd06e9dfcd38
lucas <lluc23@hotmail.com>
2022-01-23 20:17:12 +0100

    pointing to assets

--- hybrid_django_react/copy_assets.py
+++ hybrid_django_react/copy_assets.py
@@ -1,12 +1,10 @@
 import os
 from distutils.dir_util import copy_tree
-
+from pathlib import Path
 
 def copy_assets_to_current_dir():
-    file_path = os.path.realpath(__file__)
-    assets_path = os.path.join(file_path, "../assets")
+    file_path = Path(__file__).resolve().parent
+    assets_path = f'{file_path}/assets'
     files = os.listdir(assets_path)
-
     current_directory_path = os.getcwd()
-
     copy_tree(assets_path, current_directory_path)
